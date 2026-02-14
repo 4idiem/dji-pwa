6997f91791cae9312fd63c7370a37d66c7c16900
4idiem <4idiem@gmail.com>
2026-02-14 10:32:45 +0300

    бновлены изображения продуктов, добавлена папка images, обновлены иконки сравнения, добавлены теги овинка

--- src/sections/NewArrivalsSection/NewArrivalsSection.tsx
+++ src/sections/NewArrivalsSection/NewArrivalsSection.tsx
@@ -24,9 +24,9 @@ function ArrowRightIcon() {
 function CompareIcon() {
   return (
     <svg className={styles.compareIcon} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="2" y="10" width="4" height="8" rx="1" fill="#A2A6AF" />
-      <rect x="8" y="6" width="4" height="12" rx="1" fill="#C4C8D0" />
-      <rect x="14" y="2" width="4" height="16" rx="1" fill="#A2A6AF" />
+      <rect x="2" y="12" width="4" height="6" rx="1" fill="#A2A6AF" />
+      <rect x="8" y="4" width="4" height="14" rx="1" fill="#A2A6AF" />
+      <rect x="14" y="8" width="4" height="10" rx="1" fill="#C4C8D0" />
     </svg>
   );
 }
